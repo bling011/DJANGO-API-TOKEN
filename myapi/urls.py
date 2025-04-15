7ac452ad3dc9a507c2aeb119d0af82fdff61678f
RovicAlmonia <rovicmatthew0311@gmail.com>
2025-04-15 20:06:00 +0800

    Add gunicorn to requirements

--- myapi/urls.py
+++ myapi/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from .views import HelloWorld
 from .views import SecureHelloView
 from rest_framework.authtoken.views import obtain_auth_token
+from django.http import JsonResponse
 
+def home(request):
+    return JsonResponse({'status': 'API is running 🚀'})
 
 urlpatterns = [
+    path('', home),  # Now visiting / will return a friendly message
     path('hello/', HelloWorld.as_view()), 
     path('secure-hello/', SecureHelloView.as_view()),
     path('api-token-auth/', obtain_auth_token), 
 ]
+
